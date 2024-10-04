d09176b8918c215b1099861c43f9d314d7e14068
juangtddev <juan.gtd.dev@gmail.com>
2024-10-04 18:50:06 -0300

    first version - waiting for improvements

---
+++ script.js
@@ -0,0 +1,30 @@
+const apiKey = "c4a7dd72d7b28db648450067d3e1534c";
+const apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        let data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML =
+            Math.trunc(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML =
+            data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        weatherIcon.src = "assets/images/" + data.weather[0].main + ".png";
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+
+searchBtn.addEventListener("click", () => checkWeather(searchBox.value));
